b459f03dc282c0193ce1749a9d0df33c5305764e
NickSittipon <sittipon5725@gmail.com>
2025-05-06 22:27:57 +0700

    feat: implement GET and POST handling in NetworkApiService

---
+++ lib/data/network/NetworkApiService.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_mvvm_provider/data/app_exceptions.dart';
+import 'package:flutter_mvvm_provider/data/network/BaseApiServices.dart';
+
+class NetworkApiService extends BaseApiServices {
+  @override
+  Future getGetApiResponse(String url) async {
+    dynamic responseJson;
+    try {
+      final response = await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException("Error During Communication: $url");
+    }
+    return responseJson;
+  }
+
+  @override
+  Future getPostApiResponse(String url, dynamic data) async {
+    dynamic responseJson;
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: data,
+      ).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException("Error During Communication: $url");
+    }
+    return responseJson;
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        jsonDecode(response.body);
+        return jsonDecode(response.body);
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 500:
+      case 404:
+        throw UnauthorisedException(response.body.toString());
+      
+      default:
+        throw FetchDataException(
+          "Error occurred while communicating with server with status code: ${response.statusCode}");
+    }
+  }
+}
